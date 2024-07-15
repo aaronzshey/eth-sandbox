ae7ae6cb17af497745c9d688881566e7467e7369
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-07-14 23:27:03 -0700

    no private keys

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
-import { setBalance, mine } from "@nomicfoundation/hardhat-network-helpers";
+import helpers, {
+  setBalance,
+  mine,
+} from "@nomicfoundation/hardhat-network-helpers";
+import { ethers } from "ethers";
+import hardhat from "hardhat";
+import * as contract from "./artifacts/contracts/MyNFT.sol/MyNFT.json" with { type: "json" };
 
 async function main() {
+  const myAddress = "0x06b0ED5338e36623b859081B0692F7dE33aF67E5";
+
   await setBalance(
     "0x06b0ED5338e36623b859081B0692F7dE33aF67E5",
-    999999999999999999999999999999999999999999999999999999999999,
+    9999999999999999999999999999999999999999999999999999999,
+  );
+
+  await mine();
+  console.log("mined");
+
+  const impersonatedSigner =
+    await hardhat.ethers.getImpersonatedSigner(myAddress);
+
+  const myContractFactory = new ethers.ContractFactory(
+    contract.default.abi,
+    contract.default.bytecode,
+    impersonatedSigner,
+  );
+
+  const contractFactoryTxn = await myContractFactory.deploy(myAddress);
+  await contractFactoryTxn.waitForDeployment();
+  const factoryAddress = await contractFactoryTxn.getAddress();
+
+  const myNFTContract = new ethers.Contract(
+    factoryAddress,
+    contract.default.abi,
+    impersonatedSigner,
+  );
+
+  let txn = await myNFTContract.safeMint(
+    impersonatedSigner.address,
+    "ipfs://deadlink",
+  );
+  await txn.wait();
+  console.log(txn);
+
+  await mine();
+  //console.log(factoryAddress);
+  await setBalance(
+    "0x06b0ED5338e36623b859081B0692F7dE33aF67E5",
+    65170898460000000,
   );
 
   await mine();
