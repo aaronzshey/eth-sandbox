cc01e51e3630899fe58510e00c02dca23006ceb2
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-07-19 14:32:44 -0700

    just kidding i don't want custom types

--- utils/deploy.js
+++ utils/deploy.js
@@ -6,6 +6,6 @@ import { ethers } from "ethers";
 import hardhat from "hardhat";
 import * as contract from "./artifacts/contracts/MyNFT.sol/MyNFT.json" with { type: "json" };
 
-export default async function deployContract(address) {
+export default async function deployContract(address: string) {
 
 }
